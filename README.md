b09c144d45319406e54befeebfa2e3701c55753c
Haochen Pan <haochen.pan@bc.edu>
2021-03-17 21:05:47 +0800

    improved erun and README

--- README.md
+++ README.md
@@ -6,4 +6,11 @@
 - erun.sh
 - ekill.sh
 - eclear.sh
-- eanalysis.py
+- eanalysis.py: analysis the network usage over time on a single machine
+
+# The workflow
+- in this folder, do `. ecompile.sh` to compile the master, server, and client code
+- adjust parameters in `erun.sh` and do `. erun.sh` to run EPaxos
+- when an EPaxos run is over, run `python3 eanalysis-log.py` to see the throughput
+- run `. eclear.sh` to remove logs and those (empty) EPaxos storage files
+- If EPaxos goes wrong at any time, run `. ekill.sh` to kill  master, server, and client instances.
--- erun.sh
+++ erun.sh
@@ -1,26 +1,27 @@
 # 1. user configurations
 maddr=localhost # EPaxos master address
 mport=17070     # EPaxos master port
-NS=5            # the number of servers
-NC=10            # the number of clients
-reqsNb=10000000  # the number of unbatched-requests per client (q)
-cbatch=1000      # the number of requests in a client batch (cb)
-writes=50       # the percentage of client write operations / read & write operations (w)
-conflicts=0     # the percentage fo client read & write conflicts (c)
+NS=3            # the number of servers (NS)
+NC=1            # the number of clients (NC)
+reqsNb=1000     # the number of unbatched-requests per client (re)
+cbatch=1        # the number of requests in a client batch (cb)
+writes=50       # the percentage of client write operations / read & write operations (wr)
+conflicts=0     # the percentage fo client read & write conflicts (cf)
 serverP=2       # server's GOMAXPROCS (sp)
 clientP=1       # client's GOMAXPROCS (cp)
-thrifty=false   # EPaxos: "Use only as many messages as strictly required for inter-replica communication" (t)
+thrifty=true    # EPaxos: "Use only as many messages as strictly required for inter-replica communication" (th)
 
 # 2. runtime variables (no need to modify)
 rounds=$(($reqsNb / $cbatch)) # the number of rounds = reqsNb / cbatch
 efolder=${HOME}/go/src/epaxos
 mkdir -p ${efolder}/logs
+log_file_path_head=${efolder}/logs/NS${NS}-NC${NC}-re${reqsNb}-cb${cbatch}-wr${writes}-cf${conflicts}-sp${serverP}-cp${clientP}-th${thrifty}
 servers=() # server PIDs
 clients=() # client PIDs
 
 # 3. start the EPaxos leader
 emaster -port=$mport -N=$NS \
-    >${efolder}/logs/NS${NS}-NC${NC}-q${reqsNb}-cb${cbatch}-w${writes}-c${conflicts}-sp${serverP}-cp${clientP}-t${thrifty}-master.out 2>&1 &
+    >${log_file_path_head}-master.out 2>&1 &
 servers[0]=$!
 echo "master started"
 sleep 1
@@ -28,7 +29,7 @@ sleep 1
 # 4. start EPaxos servers
 for i in $(seq $NS); do
     eserver -port=$(($mport + $i)) -maddr=$maddr -mport=$mport -addr=localhost -e=true -p=$serverP -thrifty=${thrifty} \
-        >${efolder}/logs/NS${NS}-NC${NC}-q${reqsNb}-cb${cbatch}-w${writes}-c${conflicts}-sp${serverP}-cp${clientP}-t${thrifty}-server$(($i - 1)).out 2>&1 &
+        >${log_file_path_head}-server$(($i - 1)).out 2>&1 &
     servers[i]=$!
 done
 echo "servers started"
@@ -37,7 +38,7 @@ sleep 3
 # 5. start EPaxos clients
 for i in $(seq $NC); do
     eclient -maddr=$maddr -mport=$mport -q=$reqsNb -w=$writes -e=true -r=$rounds -p=$clientP -c=$conflicts \
-        >${efolder}/logs/NS${NS}-NC${NC}-q${reqsNb}-cb${cbatch}-w${writes}-c${conflicts}-sp${serverP}-cp${clientP}-t${thrifty}-client$(($i - 1)).out 2>&1 &
+        >${log_file_path_head}-client$(($i - 1)).out 2>&1 &
     clients[$(($i - 1))]=$!
 done
 echo "clients started"
@@ -54,7 +55,7 @@ done
 
 # 7. conduct monitoring (on Mac)
 nettop -P -l 0 -J time,bytes_in,bytes_out ${pids} \
-    >${efolder}/logs/NS${NS}-NC${NC}-q${reqsNb}-cb${cbatch}-w${writes}-c${conflicts}-sp${serverP}-cp${clientP}-t${thrifty}-nettop.out 2>&1 &
+    >${log_file_path_head}-nettop.out 2>&1 &
 netPID=$!
 
 # 8. waits the clients to exit
