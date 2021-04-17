df016612ea0e331f863d2f1982dd9089e3b987f8
Jesse Tuglu <jktuglu@gmail.com>
2021-04-16 21:57:53 -0400

    fix

--- kill.sh
+++ kill.sh
@@ -1,12 +1,12 @@
-pgrep -af /root/go/src/epaxos/bin/emaster | while read -r pid cmd ; do
+pgrep -af /root/go/src/epaxos/bin/master | while read -r pid cmd ; do
      echo "pid: $pid, cmd: $cmd"
     kill -9 $pid > /dev/null 2>&1
 done
-pgrep -af /root/go/src/epaxos/bin/eserver | while read -r pid cmd ; do
+pgrep -af /root/go/src/epaxos/bin/server | while read -r pid cmd ; do
      echo "pid: $pid, cmd: $cmd"
     kill -9 $pid > /dev/null 2>&1
 done
-pgrep -af /root/go/src/epaxos/bin/eclient | while read -r pid cmd ; do
+pgrep -af /root/go/src/epaxos/bin/client | while read -r pid cmd ; do
      echo "pid: $pid, cmd: $cmd"
     kill -9 $pid > /dev/null 2>&1
 done
