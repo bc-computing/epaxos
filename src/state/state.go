8f84db5651988b5ef8bc8d04b1df4a23490c1465
terry891 <71469277+terry891@users.noreply.github.com>
2021-05-04 21:57:07 -0400

    update for 256B

--- src/state/state.go
+++ src/state/state.go
@@ -26,8 +26,38 @@ type Key int64
 
 type Command struct {
 	Op Operation
-	K  Key
-	V  Value
+	K  Key 		// key = 8 bytes
+ 	V1  Value 	// 31 values * 8 bytes = 248 bytes
+ 	V2  Value  	// total = 256 bytes
+ 	V3  Value
+ 	V4  Value
+ 	V5  Value
+ 	V6  Value
+ 	V7  Value
+ 	V8  Value
+ 	V9  Value
+ 	V10  Value
+ 	V11  Value
+ 	V12  Value
+ 	V13  Value
+ 	V14  Value
+ 	V15  Value
+ 	V16  Value
+ 	V17  Value
+ 	V18  Value
+ 	V19  Value
+	V20  Value
+ 	V21  Value
+ 	V22  Value
+ 	V23  Value
+ 	V24  Value
+ 	V25  Value
+ 	V26  Value
+ 	V27  Value
+ 	V28  Value
+ 	V29  Value
+	V30  Value
+ 	V31  Value
 }
 
 type State struct {
